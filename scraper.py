0867a8041fb818f3e5c18ef98c849826ab9a5981
Jacob <jjmarsz2@uiuc.edu>
2019-07-20 09:46:54 -0500

    updated db table. started parsing the match links, need to find how to get # of matches

--- scraper.py
+++ scraper.py
@@ -16,30 +16,30 @@ def create_connection(db_file):
         conn.close()
 
 def error(msg):
-	print('[ERROR] :: ' + msg)
+    print('[ERROR] :: ' + msg)
 
 # Validate number of args
 if(len(sys.argv) > 3):
-    error("Too many arguments :: Format is python scrapper (url to team's game series) (number of games)")
+    error("Too many arguments :: Format is python scrapper (url to team's game series) (number of series)")
     sys.exit()
 if(len(sys.argv) < 2):
-    error("Too little arguments :: Format is python scrapper (url to team's game series) (number of games)")
+    error("Too little arguments :: Format is python scrapper (url to team's game series) (number of series)")
     sys.exit()
 
 # Validate args
 team_series_url = sys.argv[1]
 
 if(len(sys.argv) <= 2):
-	num_games = 20
+    num_series = 20
 else:
-	num_games = sys.argv[2]
+    num_series = sys.argv[2]
 
 if not ('https://www.dotabuff.com/esports/teams/' in team_series_url):
     error('Malformed URL :: Must be https://www.dotabuff.com/esports/teams/...') 
     sys.exit()
 
 # Connect to the Database
-create_connection("stats.db")
+conn = sqlite3.connect("stats.db")
 cur = conn.cursor()
 
 # Some useful constants
@@ -50,22 +50,32 @@ hdr = { 'User-Agent' : 'fantasy stats' }
 source = requests.get(team_series_url, headers=hdr)
 soup = bs.BeautifulSoup(source.text, 'lxml')
 
-#obtain the series
-games = []
+# Obtain the selected series
+series = []
 next_page = '2'
 next_page_link = ''
-page_soup = soup
-while len(games) < int(num_games):
-    for url in page_soup.find_all('a'):
+while len(series) < int(num_series):
+    for url in soup.find_all('a'):
         if '/esports/series/' in url.get('href'):
-            games.append(db + url.get('href'))
+            series.append(db + url.get('href'))
         if 'page=' + next_page == url.get('href')[len(url.get('href'))-6:len(url.get('href'))]:
             next_page_link = db + url.get('href')
     #set up the variables for the next page
     next_page_link = requests.get(next_page_link, headers=hdr)
-    page_soup = bs.BeautifulSoup(next_page_link.text, 'lxml')
+    soup = bs.BeautifulSoup(next_page_link.text, 'lxml')
     next_page = str(int(next_page) + 1)
 
-games = games[0:int(num_games)]
-for series_link in games:
-    print(series_link)
+# Loop through every series and store the data
+series = series[0:int(num_series)]
+for series_link in series:
+    # First connect to new webpage with a new soup
+    source = requests.get(series_link, headers=hdr)
+    soup = bs.BeautifulSoup(source.text, 'lxml')
+
+    # Find all the games
+    for of in soup.find_all('Best of '):
+    	print(of)
+    for url in soup.find_all('a'):
+    	if 'matches/' in url.get('href'):
+        	print(url.get('href'))
+
