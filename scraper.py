e74be2b2b3d4195e3d3929115f30e706d64356ff
Jacob <jjmarsz2@uiuc.edu>
2019-07-19 23:51:38 -0500

    added error func, input validation, and series link extractor. obtains the link to the # of requested series, defaults to 20 if not provided

--- scraper.py
+++ scraper.py
@@ -4,27 +4,52 @@ import requests
 import sys
 import sqlite3
 
-#Format is 'python scrapper (url) (number of games)''
 
+def error(msg):
+	print('[ERROR] :: ' + msg)
+
+# Validate number of args
 if(len(sys.argv) > 3):
-	print("Too many arguments")
+    error("Too many arguments :: Format is python scrapper (url to team's game series) (number of games)")
+    sys.exit()
+if(len(sys.argv) < 2):
+    error("Too little arguments :: Format is python scrapper (url to team's game series) (number of games)")
+    sys.exit()
 
-url = sys.argv[1]
-numGames = sys.argv[2]
+# Validate args
+team_series_url = sys.argv[1]
 
-print(type(int(sys.argv[2])))
+if(len(sys.argv) <= 2):
+	num_games = 20
+else:
+	num_games = sys.argv[2]
+
+if not ('https://www.dotabuff.com/esports/teams/' in team_series_url):
+    error('Malformed URL :: Must be https://www.dotabuff.com/esports/teams/...') 
+    sys.exit()
 
 db = 'https://www.dotabuff.com'
 hdr = { 'User-Agent' : 'fantasy stats' }
-source = requests.get('https://www.dotabuff.com/esports/teams/2586976-og/series', headers=hdr)
+source = requests.get(team_series_url, headers=hdr)
 
 soup = bs.BeautifulSoup(source.text, 'lxml')
 
 #obtain the series
 games = []
+next_page = '2'
+next_page_link = ''
+page_soup = soup
+while len(games) < int(num_games):
+    for url in page_soup.find_all('a'):
+        if '/esports/series/' in url.get('href'):
+            games.append(db + url.get('href'))
+        if 'page=' + next_page == url.get('href')[len(url.get('href'))-6:len(url.get('href'))]:
+            next_page_link = db + url.get('href')
+    #set up the variables for the next page
+    next_page_link = requests.get(next_page_link, headers=hdr)
+    page_soup = bs.BeautifulSoup(next_page_link.text, 'lxml')
+    next_page = str(int(next_page) + 1)
 
-for url in soup.find_all('a'):
-    if '/esports/series' in url.get('href'):
-    	games.append(db + url.get('href'))
-
-print(games)
+games = games[0:int(num_games)]
+for series_link in games:
+    print(series_link)
