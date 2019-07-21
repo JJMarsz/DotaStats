d26e4c3c1e217e2f1f260cddf66c1a4c8d53535d
Jacob <jjmarsz2@uiuc.edu>
2019-07-21 14:51:43 -0500

    added more subroutines to simplifiy main

--- scraper.py
+++ scraper.py
@@ -4,35 +4,82 @@ import requests
 import sys
 import sqlite3
 
+# Control flags
+fail_error = 0
+log_lvl = 0
+
 # Some useful constants
 db = 'https://www.dotabuff.com'
 hdr = { 'User-Agent' : 'fantasy stats' }
 
+#
+# Subroutines
+#
 def error(msg):
     print('[ERROR] :: ' + msg)
+    if fail_error:
+        sys.exit()
 
+# requests a soup of the url
 def getSoup(url):
     source = requests.get(url, headers=hdr)
     return bs.BeautifulSoup(source.text, 'lxml')
 
-#def getSeriesLinks():
+# retrieves the num_series amount of series_links
+def getSeriesLinks(soup, num_series):
+    series = []
+    next_page = '2'
+    next_page_link = ''
+    while len(series) < int(num_series):
+        for url in soup.find_all('a'):
+            if '/esports/series/' in url.get('href'):
+                series.append(db + url.get('href'))
+            if 'page=' + next_page == url.get('href')[len(url.get('href'))-6:len(url.get('href'))]:
+                next_page_link = db + url.get('href')
+        #set up the variables for the next page
+        soup = getSoup(next_page_link)
+        next_page = str(int(next_page) + 1)
+    return series
 
-# Validate number of args
+# determines the type of a series
+def getBestOf(soup):
+    bo = -1
+    if 'Best of ' in soup.get_text():
+        idx = soup.get_text().find('Best of ')
+        bo = int(soup.get_text()[idx + 8])
+    else:
+        error('Cannot find the amount of games in the series')
+    return bo
+
+# returns the amount of matches in the series
+def getMatchLinks(soup):
+    match_links = []
+    game_no = 1
+    while ("Game " + str(game_no) + ":") in soup.getText():
+    	game_no += 1
+    game_no -= 1
+    for url in soup.find_all('a'):
+        if '/matches/' in url.get('href'):
+            match_links.append(db + url.get('href'))
+    match_links = match_links[0:game_no]
+    return match_links
+
+#
+# Main
+#
+
+# Validate args
 if(len(sys.argv) > 3):
     error("Too many arguments :: Format is python scrapper (url to team's game series) (number of series)")
     sys.exit()
 if(len(sys.argv) < 2):
     error("Too little arguments :: Format is python scrapper (url to team's game series) (number of series)")
     sys.exit()
-
-# Validate args
 team_series_url = sys.argv[1]
-
 if(len(sys.argv) <= 2):
     num_series = 20
 else:
     num_series = sys.argv[2]
-
 if not ('https://www.dotabuff.com/esports/teams/' in team_series_url):
     error('Malformed URL :: Must be https://www.dotabuff.com/esports/teams/...') 
     sys.exit()
@@ -41,23 +88,11 @@ if not ('https://www.dotabuff.com/esports/teams/' in team_series_url):
 conn = sqlite3.connect("stats.db")
 cur = conn.cursor()
 
-
 # Soup and site connections
 soup = getSoup(team_series_url)
 
 # Obtain the selected series
-series = []
-next_page = '2'
-next_page_link = ''
-while len(series) < int(num_series):
-    for url in soup.find_all('a'):
-        if '/esports/series/' in url.get('href'):
-            series.append(db + url.get('href'))
-        if 'page=' + next_page == url.get('href')[len(url.get('href'))-6:len(url.get('href'))]:
-            next_page_link = db + url.get('href')
-    #set up the variables for the next page
-    soup = getSoup(next_page_link)
-    next_page = str(int(next_page) + 1)
+series = getSeriesLinks(soup, num_series)
 
 # Loop through every series and store the data
 series = series[0:int(num_series)]
@@ -68,7 +103,7 @@ for series_link in series:
 
     # Set up loop variables
     soup = getSoup(series_link)
-    bo = -1
+    bo = getBestOf(soup)
     match_links = []
     v_idx = int(str(soup.title).find(' vs '))
     end_idx = int(str(soup.title).find(' - '))
@@ -76,33 +111,21 @@ for series_link in series:
     team_2 = str(soup.title)[v_idx+4:end_idx]
     team_1_wins = 0
     team_2_wins = 0
-    # Find the type of series
-    if 'Best of ' in soup.get_text():
-        idx = soup.get_text().find('Best of ')
-        bo = int(soup.get_text()[idx + 8])
-    else:
-        error('Cannot find the amount of games in the series')
 
-    # Get every match link in the page
-    for url in soup.find_all('a'):
-        if '/matches/' in url.get('href'):
-            match_links.append(db + url.get('href'))
+    match_links = getMatchLinks(soup)
 
     # Count every match up until a team wins 'bo' amount
     for match_link in match_links:
-        if team_1_wins == int(bo/2)+1 or team_2_wins == int(bo/2)+1 or (bo == 2 and team_1_wins + team_2_wins == bo):
-            break
-        # Set up loop variables
         soup = getSoup(match_link)
 
         # Find out who won
-        v_idx = soup.get_text().find(" Victory!")
+        v_idx = soup.get_text().find(' Victory!')
         team_1_idx = soup.get_text().find(team_1, v_idx-20, v_idx)
         team_2_idx = soup.get_text().find(team_2, v_idx-20, v_idx)
         if v_idx - team_1_idx < v_idx - team_2_idx:
-        	team_1_wins += 1
+            team_1_wins += 1
         else:
-        	team_2_wins += 1
+            team_2_wins += 1
 
     series_idx = series_link.find('/series/') + 8
     end_idx = series_link.find('-')
