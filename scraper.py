5c90d48e8fff043c352f91dfc378c5e286046383
Jacob <jjmarsz2@uiuc.edu>
2019-07-25 19:36:34 -0500

    added better logs and sql queries into data base

--- scraper.py
+++ scraper.py
@@ -6,8 +6,10 @@ import json
 
 # Control flags
 fail_error = 0
-log_lvl = 0
+log_lvl = 2 #0-nothing, 1-ERROR,2-INFO,3-DEBUG
+phase_lvl = 'ALL' #ALL, 1, 2, 3
 cache_data = 1
+db_file = 'stats.db'
 
 # Some useful constants
 start_2018 = 1513728000
@@ -32,16 +34,19 @@ heroes = ['Abaddon', 'Alchemist', 'Axe', 'Beastmaster', 'Brewmaster', 'Bristleba
 # Subroutines
 #
 def error(msg):
-    print('[ERROR] :: ' + msg)
+    if log_lvl > 0: print('[ERROR] :: ' + msg)
     if fail_error:
         sys.exit()
 
-def log(msg):
-    print('[INFO] :: ' + msg)
+def info(msg):
+    if log_lvl > 1: print('[INFO] :: ' + msg)
+
+def debug(msg):
+    if log_lvl > 2: print('[DEBUG] :: ' + msg)
 
 # appends the key to the string
 def apiCall(url, key):
-	return json.loads(requests.get(od + url + '?api_key=' + key, headers=hdr).text)
+    return json.loads(requests.get(od + url + '?api_key=' + key, headers=hdr).text)
 
 # returns upper bound on number of matches
 def getMatchLinks(team_id, num_tourneys, key):
@@ -50,12 +55,12 @@ def getMatchLinks(team_id, num_tourneys, key):
     t_count = 0
     last_t = ''
     for i in range(int(num_tourneys)*20):
-    	if match_json[i]['league_name'] != last_t:
-    		last_t = match_json[i]['league_name']
-    		t_count += 1
-    		if t_count > int(num_tourneys):
-    			break
-    	match_links.append(match_json[i]['match_id'])
+        if match_json[i]['league_name'] != last_t:
+            last_t = match_json[i]['league_name']
+            t_count += 1
+            if t_count > int(num_tourneys):
+                break
+        match_links.append(match_json[i]['match_id'])
     return match_links
 
 # parse input params
@@ -67,14 +72,14 @@ def getMatchLinks(team_id, num_tourneys, key):
 # team_idN num_tournaments
 # (MUST HAVE NEW LINE AT END)
 def parseParams(params):
-	dic = {'key' : params[:params.find('\n')]}
-	params = params[params.find('\n')+1:]
-	while params.find('\n') != -1:
-		dic[params[:params.find(' ')]] = params[params.find(' ')+1:params.find('\n')]
-		params = params[params.find('\n')+1:]
-	return dic
-	
-#	
+    dic = {'key' : params[:params.find('\n')]}
+    params = params[params.find('\n')+1:]
+    while params.find('\n') != -1:
+        dic[params[:params.find(' ')]] = params[params.find(' ')+1:params.find('\n')]
+        params = params[params.find('\n')+1:]
+    return dic
+    
+#    
 # Main
 #
 
@@ -91,28 +96,42 @@ param_file.close()
 key = params['key']
 params.pop('key')
 # Connect to the Database
-conn = sqlite3.connect("stats.db")
+info('Connecting to the DB...')
+conn = sqlite3.connect(db_file)
 cur = conn.cursor()
-# loop on each team
-for k in params.keys():
-	match_ids = getMatchLinks(k, params[k], key)
-	for match_id in match_ids:
-		match_data = apiCall('matches/' + str(match_id), key)
-		player_dp = {}
+if phase_lvl in ['ALL', '1']:
+    info('Phase 1 - Scraping data from OpenDota')
+    # loop on each team
+    for k in params.keys():
+        info('Scraping team ' + k + '\'s matches')
+        match_ids = getMatchLinks(k, params[k], key)
+        info('Found ' + str(len(match_ids)) + ' matches in last ' + params[k] + ' tournaments')
+        for match_id in match_ids:
+            match_data = apiCall('matches/' + str(match_id), key)
+            player_dp = {}
 
-		# gather match stats
-		match_dp = [int(match_data['series_id']), int(match_data['match_id']), int(match_data['radiant_win']), int(match_data['dire_team_id']), \
-					int(match_data['radiant_team_id']), int(match_data['duration']), match_data['league']['name'], int(match_data['start_time'])-start_2018]
-		print(match_dp)
+            # gather match stats
+            match_dp = [int(match_data['match_id']), int(match_data['series_id']), int(match_data['radiant_win']), int(match_data['dire_team_id']), \
+                        int(match_data['radiant_team_id']), int(match_data['duration']), match_data['league']['name'], int(match_data['start_time'])-start_2018]
+            debug(match_dp)
+            cur.execute('SELECT * FROM match_data WHERE match_id = ?', [match_dp[0],])
+            if len(cur.fetchall()) == 0:
+                cur.execute('INSERT INTO match_data VALUES (?,?,?,?,?,?,?,?)', match_dp)
+            # gather player stats
+            for player in match_data['players']:
+                player_dp = [int(player['account_id']), int(match_data['match_id']), int(player['hero_id']), int(player['kills']), int(player['deaths']), 
+                            int(player['last_hits']) + int(player['denies']), int(player['gold_per_min']), int(player['tower_kills']), \
+                            int(player['roshan_kills']), float(player['teamfight_participation']), int(player['obs_placed']), \
+                            int(player['camps_stacked']), int(player['rune_pickups']), int(player['firstblood_claimed']), float(player['stuns'])]
+                debug(player_dp)
 
-		# gather player stats
-		for player in match_data['players']:
-			player_dp[player['account_id']] = [int(match_data['match_id']), int(player['hero_id']), int(player['kills']), int(player['deaths']), int(player['last_hits']) + \
-												int(player['denies']), int(player['gold_per_min']), int(player['tower_kills']), \
-												int(player['roshan_kills']), float(player['teamfight_participation']), int(player['obs_placed']), \
-												int(player['camps_stacked']), int(player['rune_pickups']), int(player['firstblood_claimed']), float(player['stuns'])]
-			print(str(player['account_id']) + " : " + str(player_dp[player['account_id']]))
+                cur.execute('SELECT * FROM player_data WHERE account_id = ?', [player_dp[0],])
+                if len(cur.fetchall()) == 0:
+                    cur.execute('INSERT INTO player_data VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', player_dp)
+            debug('Committing data points for match ' + str(match_data['match_id']))
+            conn.commit()
 
-		# BREAK EARLY TO LIMIT API CALLS FOR NOW
+info('Phase 2 - Aggregating data')
 
-		break
+info('Phase 3 - Querying data')
+conn.close()
