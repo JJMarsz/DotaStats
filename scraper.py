63d59b9c54bdb6daf273c2308b2a5e89f5b8754f
Jacob <jjmarsz2@uiuc.edu>
2019-07-28 15:40:51 -0500

    added hero_summary

--- scraper.py
+++ scraper.py
@@ -88,33 +88,33 @@ def extractColumn(q, i=0):
     return data
 
 # populates a summary table for the specific outline
-def summarize(table_name, pre_stats, all_flag, query_tail, params):
-    cur.execute('SELECT * FROM ' + table_name)
-    if len(cur.fetchall()) > 0:
-        info('Flushing ' + table_name + ' table...')
-        cur.execute('DELETE FROM ' + table_name)
-        conn.commit()
-    info('Populating ' + table_name + ' table...')
-    cur.execute('SELECT * FROM player_lookup')
-    players = cur.fetchall()
-    for player in players:
-        cur.execute('SELECT AVG(kills), AVG(deaths), AVG(lh_and_d), AVG(gpm), AVG(tower_kills), AVG(roshan_kills), AVG(teamfight), \
-            AVG(obs_placed), AVG(camps_stacked), AVG(rune_pickups), AVG(first_blood), AVG(stuns), AVG(duration) FROM player_data AS pd, ' + query_tail, params)
-        stats = cur.fetchall()
+#def summarize(table_name, query_tail, params, all_flag=1):
+    # cur.execute('SELECT * FROM ' + table_name)
+    # if len(cur.fetchall()) > 0:
+    #     info('Flushing ' + table_name + ' table...')
+    #     cur.execute('DELETE FROM ' + table_name)
+    #     conn.commit()
+    # info('Populating ' + table_name + ' table...')
+    # cur.execute('SELECT * FROM player_lookup')
+    # players = cur.fetchall()
+    # for player in players:
+    #     cur.execute('SELECT AVG(kills), AVG(deaths), AVG(lh_and_d), AVG(gpm), AVG(tower_kills), AVG(roshan_kills), AVG(teamfight), \
+    #         AVG(obs_placed), AVG(camps_stacked), AVG(rune_pickups), AVG(first_blood), AVG(stuns), AVG(duration) FROM player_data AS pd, ' + query_tail, params)
+    #     stats = cur.fetchall()
 
-        stat_dp = stats[0][0]*points['kills'] + stats[0][1]*points['deaths'] + 3 + stats[0][2]*points['lh_and_d'] + stats[0][3]*points['gpm'] + \
-                stats[0][4]*points['tower_kills'] + stats[0][5]*points['roshan_kills'] + stats[0][6]*points['teamfight'] + stats[0][7]*points['obs_placed'] + \
-                stats[0][8]*points['camps_stacked'] + stats[0][9]*points['rune_pickups'] + stats[0][10]*points['first_blood'] + stats[0][11]*points['stuns']
-        stats[0] = list(stats[0])
-        for i in range(len(stats[0])):
-            stats[0][i] = round(stats[0][i], 4)
-        stat_dp = round(stat_dp, 4)
-        cur.execute('INSERT INTO ' + table_name + ' VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', \
-                                                                [player[1], roles[player[3]], round((stat_dp*60)/stats[0][12], 4), stat_dp, stats[0][0]*points['kills'], stats[0][1]*points['deaths'] + 3, \
-                                                                stats[0][2]*points['lh_and_d'], stats[0][3]*points['gpm'], stats[0][4]*points['tower_kills'], \
-                                                                stats[0][5]*points['roshan_kills'], stats[0][6]*points['teamfight'], stats[0][7]*points['obs_placed'], \
-                                                                stats[0][8]*points['camps_stacked'], stats[0][9]*points['rune_pickups'], stats[0][10]*points['first_blood'], \
-                                                                stats[0][11]*points['stuns']])
+    #     stat_dp = stats[0][0]*points['kills'] + stats[0][1]*points['deaths'] + 3 + stats[0][2]*points['lh_and_d'] + stats[0][3]*points['gpm'] + \
+    #             stats[0][4]*points['tower_kills'] + stats[0][5]*points['roshan_kills'] + stats[0][6]*points['teamfight'] + stats[0][7]*points['obs_placed'] + \
+    #             stats[0][8]*points['camps_stacked'] + stats[0][9]*points['rune_pickups'] + stats[0][10]*points['first_blood'] + stats[0][11]*points['stuns']
+    #     stats[0] = list(stats[0])
+    #     for i in range(len(stats[0])):
+    #         stats[0][i] = round(stats[0][i], 4)
+    #     stat_dp = round(stat_dp, 4)
+    #     cur.execute('INSERT INTO ' + table_name + ' VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', \
+    #                                                             [player[1], roles[player[3]], round((stat_dp*60)/stats[0][12], 4), stat_dp, stats[0][0]*points['kills'], stats[0][1]*points['deaths'] + 3, \
+    #                                                             stats[0][2]*points['lh_and_d'], stats[0][3]*points['gpm'], stats[0][4]*points['tower_kills'], \
+    #                                                             stats[0][5]*points['roshan_kills'], stats[0][6]*points['teamfight'], stats[0][7]*points['obs_placed'], \
+    #                                                             stats[0][8]*points['camps_stacked'], stats[0][9]*points['rune_pickups'], stats[0][10]*points['first_blood'], \
+    #                                                             stats[0][11]*points['stuns']])
 
 #    
 # Main
@@ -283,7 +283,32 @@ if 4 in exec_phase:
                                                                 stats[0][8]*points['camps_stacked'], stats[0][9]*points['rune_pickups'], stats[0][10]*points['first_blood'], \
                                                                 stats[0][11]*points['stuns']])
     conn.commit()
-if 5 in exec_phase:
-    info('Phase 5 - Querying tables in DB')
+
+    cur.execute('SELECT * FROM hero_summary')
+    if len(cur.fetchall()) > 0:
+        info('Flushing hero_summary table...')
+        cur.execute('DELETE FROM hero_summary')
+        conn.commit()
+    info('Populating hero_summary table...')
+    cur.execute('SELECT * FROM hero_lookup')
+    heroes = cur.fetchall()
+    for hero in heroes:
+        cur.execute('SELECT AVG(kills), AVG(deaths), AVG(lh_and_d), AVG(gpm), AVG(tower_kills), AVG(roshan_kills), AVG(teamfight), \
+            AVG(obs_placed), AVG(camps_stacked), AVG(rune_pickups), AVG(first_blood), AVG(stuns), AVG(duration) FROM player_data AS pd, hero_lookup AS hl, match_data AS MD WHERE md.match_id = pd.match_id AND pd.hero_id = hl.hero_id AND pd.hero_id = ?', [hero[0],])
+        stats = cur.fetchall()
+        stat_dp = stats[0][0]*points['kills'] + stats[0][1]*points['deaths'] + 3 + stats[0][2]*points['lh_and_d'] + stats[0][3]*points['gpm'] + \
+                stats[0][4]*points['tower_kills'] + stats[0][5]*points['roshan_kills'] + stats[0][6]*points['teamfight'] + stats[0][7]*points['obs_placed'] + \
+                stats[0][8]*points['camps_stacked'] + stats[0][9]*points['rune_pickups'] + stats[0][10]*points['first_blood'] + stats[0][11]*points['stuns']
+        stats[0] = list(stats[0])
+        for i in range(len(stats[0])):
+            stats[0][i] = round(stats[0][i], 4)
+        stat_dp = round(stat_dp, 4)
+        cur.execute('INSERT INTO hero_summary VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', \
+                                                                [hero[1], round((stat_dp*60)/stats[0][12], 4), stat_dp, stats[0][0]*points['kills'], stats[0][1]*points['deaths'] + 3, \
+                                                                stats[0][2]*points['lh_and_d'], stats[0][3]*points['gpm'], stats[0][4]*points['tower_kills'], \
+                                                                stats[0][5]*points['roshan_kills'], stats[0][6]*points['teamfight'], stats[0][7]*points['obs_placed'], \
+                                                                stats[0][8]*points['camps_stacked'], stats[0][9]*points['rune_pickups'], stats[0][10]*points['first_blood'], \
+                                                                stats[0][11]*points['stuns']])
+    conn.commit()
 
 conn.close()
